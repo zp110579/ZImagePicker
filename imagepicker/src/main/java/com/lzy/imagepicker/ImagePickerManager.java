7bd0ac8234c91e329679cebf89a522e146efc0b9
zp110579 <zp110579@163.com>
2019-12-19 18:15:04 +0800

    增加imageLoader方法

--- imagepicker/src/main/java/com/lzy/imagepicker/ImagePickerManager.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/ImagePickerManager.java
@@ -7,12 +7,13 @@ import android.support.v4.app.Fragment;
 import com.lzy.imagepicker.bean.CameraPickerImageSelectManager;
 import com.lzy.imagepicker.bean.ManyImageSelectManager;
 import com.lzy.imagepicker.bean.SingleImageSelectManager;
+import com.lzy.imagepicker.loader.ImageLoader;
 
 public class ImagePickerManager {
     private Activity mActivity;
 
 
-    public ImagePickerManager(Activity activity) {
+    private ImagePickerManager(Activity activity) {
         this.mActivity = activity;
     }
 
@@ -53,5 +54,11 @@ public class ImagePickerManager {
 
     public static ImagePickerManager with(DialogFragment dialogFragment) {
         return new ImagePickerManager(dialogFragment.getActivity());
+    }
+
+    public static void setImageLoader(ImageLoader imageLoader){
+        ImagePicker imagePicker = ImagePicker.getInstance();
+        imagePicker.setImageLoader(imageLoader);   //设置图片加载器
+
     }
 }
